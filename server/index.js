19e7f9447dabf3899f7200e10dfa4f70c18baaed
bdpoe <toyotdp@hotmail.com>
2025-12-07 11:14:14 -0500

    fix: update CORS for new frontend domain

--- server/index.js
+++ server/index.js
@@ -3,25 +3,27 @@ import cors from "cors";
 
 import indexRoutes from "./routes/index.routes.js";
 import taskRoutes from "./routes/tasks.routes.js";
-import authRoutes from "./routes/auth.routes.js";  // 🔹 nuevo
+import authRoutes from "./routes/auth.routes.js"; // 🔹 nuevo
 import loansRoutes from "./routes/loans.routes.js";
 
 const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+app.use(
+  cors({
+    origin: "https://stack-library-4-production-e4a1.up.railway.app",
+    
+    credentials: true,
+  })
+);
 
-app.use(cors({
-  origin: "https://overflowing-insight-production.up.railway.app",
-  credentials: true
-}));
 app.use(express.json());
 
 app.use("/api", loansRoutes);
 app.use("/api", indexRoutes);
 app.use("/api", taskRoutes);
 app.use("/api", authRoutes);
-
 
 app.listen(PORT, () => {
   console.log("Server running on port", PORT);
